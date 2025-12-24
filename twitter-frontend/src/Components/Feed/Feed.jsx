d671b41c3ccc51f78e809fa9a92e9ef59eda061f
dheeraj0808 <dheerajsingh1939@gmail.com>
2025-12-24 17:30:56 +0530

    completed left side bar

---
+++ twitter-frontend/src/Components/Feed/Feed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreatePost from './CreatePost'
+
+const Feed = () => {
+  return (
+    <div className='w-[60%]'>
+      <CreatePost />
+    </div>
+  )
+}
+
+export default Feed
---
+++ twitter-frontend/src/Components/RightSideBar/RightSideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSideBar = () => {
+  return (
+    <div className='w-[20%]'> RightSideBar</div>
+  )
+}
+
+export default RightSideBar
